391dc093c723c830770b410c631790a77965ad16
yashasreea <148551161+yashasreea@users.noreply.github.com>
2024-11-07 16:34:36 +0530

    Create ripple adder.v

---
+++ ripple adder.v
@@ -0,0 +1,23 @@
+include "full_adder.v"
+
+module adder(a, b, cin, s, cout);
+    parameter WIDTH = 4;
+
+    input[WIDTH-1:0]  a, b;
+    input       cin;
+
+    output[WIDTH-1:0] s;
+    output      cout;
+
+    wire[WIDTH:0] c;
+
+    assign c[0] = cin;
+    assign cout = c[WIDTH];
+
+    genvar i;
+
+    generate for (i = 0; i < WIDTH; i = i + 1) 
+        full_adder fa(a[i], b[i], c[i], s[i], c[i+1]);     
+    endgenerate
+    
+endmodule
